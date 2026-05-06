ed605f4d0c1900af7d0173d5333cbe29c1b47f42
zrd <zhangrongdi@1722739757700811.onaliyun.com>
2026-05-06 17:30:09 +0800

    fea: 增加小程序打包配置

--- apps/frontend/config/index.ts
+++ apps/frontend/config/index.ts
@@ -13,7 +13,7 @@ export default defineConfig<'vite'>(async (): Promise<UserConfigExport<'vite'>> 
   outputRoot: 'dist',
   framework: 'react',
   compiler: 'vite',
-  plugins: ['@tarojs/plugin-platform-h5'],
+  plugins: ['@tarojs/plugin-platform-h5', '@tarojs/plugin-platform-weapp'],
   h5: {
     publicPath: '/',
     staticDirectory: 'static',
